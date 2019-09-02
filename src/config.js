92f99b05a1b139c11295e115e3e3fb12fd3dff0e
Jenn Gonzalez <jgonz9873@gmail.com>
2019-09-01 17:57:36 -0700

    updated server link

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
     AUTH_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpcCI6WzQ4LDQ2LDQ4LDQ2LDQ4LDQ2LDQ4XSwiaXNzdWVyX2lkIjoxMDkzLCJvcmlnaW4iOiJsb2NhbGhvc3Q6MzAwMCIsImF1ZCI6Ikpva2VuIiwiZXhwIjoxNTY1NjcwNzk3LCJpYXQiOjE1NjU2NjM1OTcsImlzcyI6Ikpva2VuIiwianRpIjoiMm10NWxmcHNldWY4NDNzb2xrNzFoYm8xIiwibmJmIjoxNTY1NjYzNTk3fQ.vp89v1HubC_7gyU9P4ZHX6b26v70-Xmq8yttldjo2c8',
     API_URL: 'https://trefle.io/api/plants',
-    SERVER_ENDPOINT: process.env.SERVER_ENDPOINT || 'http://localhost:8000/api',
+    SERVER_ENDPOINT: process.env.NODE_ENV !== 'development' ? 'https://greenhouse-server23823.herokuapp.com/api' : 'http://localhost:8000/api',
     TOKEN_KEY: 'greenhouse-client-auth-token',
     USERNAME: 'username'
 }
